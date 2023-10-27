b3029c3881b46bf582e14449e003417c2ea95eb6
Jose Cáliz <jcaliz@formacionalcala.com>
2023-10-27 10:53:34 +0200

    Implementación para cambiar el title + instrucciones a seguir

--- frontend/src/app/auth/pages/edit/edit-page.component.ts
+++ frontend/src/app/auth/pages/edit/edit-page.component.ts
@@ -1,18 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { NavbarService } from 'src/app/shared/services/navbar.service';
 import { environment } from 'src/environments/environment.development';
 
 @Component({
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.css']
 })
-export class EditPageComponent {
-  // Declaración de variables
-  isPasswordChangeChecked: boolean = false;
-  // Propiedad que contiene la variable de entorno
-  public environment = environment;
+export class EditPageComponent implements OnInit{
+  // inyección de dependencias
+  private navbarService = inject(NavbarService);
+  
 
-  // Controlo el cambio del checkbox para cambiar la contraseña
-  togglePasswordChange(event: any) {
-    this.isPasswordChangeChecked = event.target.checked;
+  ngOnInit(): void {
+    this.navbarService.title.set("Editar mi cuenta"); // el título será "Editar mi cuenta"
+    this.navbarService.backUrl.set(""); // no hay url para volver atrás
   }
 }
